376e7e660ad2e69cfa9d1d701ec5ecb35275ca70
Pauline Chane <pauline.chane@gmail.com>
2021-01-09 01:21:32 -0800

    commented out card data

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 const Board = (props) => {
 
