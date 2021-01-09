64eaf49262c097d4a881ccc668863489ad204e1b
Pauline Chane <pauline.chane@gmail.com>
2021-01-08 22:11:55 -0800

    board component created

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,21 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
-      Card
+      <article className = "card__content">
+        <h2 className = "card__content-text">{props.text}</h2>
+        <p className = "card__content-emoji">{props.emoji ? emoji.getUnicode(props.emoji) : ''}</p>
+      </article>
+      <button className = "card__delete"> delete</button>
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string.isRequired
 };
 
 export default Card;
